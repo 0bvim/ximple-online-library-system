4c3209ad641aa1532f6ecd284aa0567ef22cd689
vinicius <niviciusdev@gmail.com>
2025-03-06 19:08:44 -0300

    feat: add unit tests for BookService methods

---
+++ src/test/java/com/ximple/online/library/system/service/BookServiceTest.java
@@ -0,0 +1,22 @@
+package com.ximple.online.library.system.service;
+
+import org.junit.jupiter.api.Test;
+
+class BookServiceTest {
+
+    @Test
+    void searchBooks() {
+    }
+
+    @Test
+    void createReservation() {
+    }
+
+    @Test
+    void createReview() {
+    }
+
+    @Test
+    void getBookReviews() {
+    }
+}
