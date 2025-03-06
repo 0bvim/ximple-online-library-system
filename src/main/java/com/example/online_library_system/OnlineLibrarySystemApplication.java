acabcb2a6a215278179c909b20d2eba19573c286
vinicius <niviciusdev@gmail.com>
2025-03-06 00:12:21 -0300

    initial commit

--- src/main/java/com/example/online_library_system/OnlineLibrarySystemApplication.java
+++ src/main/java/com/example/online_library_system/OnlineLibrarySystemApplication.java
@@ -2,10 +2,8 @@ package com.example.online_library_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class OnlineLibrarySystemApplication {
 
 	public static void main(String[] args) {
