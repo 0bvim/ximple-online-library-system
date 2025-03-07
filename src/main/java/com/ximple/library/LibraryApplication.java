08c0fb0d1dd2cb5e5568525c46c58e5de3cd0779
vinicius <niviciusdev@gmail.com>
2025-03-07 02:20:25 -0300

    feat: add startup logging for LibraryApplication

--- src/main/java/com/ximple/library/LibraryApplication.java
+++ src/main/java/com/ximple/library/LibraryApplication.java
@@ -12,6 +12,7 @@ public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+		logger.info("LibraryApplication started");
 	}
 
 }
