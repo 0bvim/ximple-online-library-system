1d032f7224a2983df16b8f6084d41002ee787b78
vinicius <niviciusdev@gmail.com>
2025-03-08 01:19:58 -0300

    feat: add JSON property annotation to BookDTO for read-only access to ID

--- src/main/java/com/ximple/library/dto/BookDTO.java
+++ src/main/java/com/ximple/library/dto/BookDTO.java
@@ -1,8 +1,11 @@
 package com.ximple.library.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public record BookDTO(
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         UUID id,
         String title,
         String author,
