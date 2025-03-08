d57ef5b64c86311e54219c891ac1918a4653082a
vinicius <niviciusdev@gmail.com>
2025-03-08 02:29:08 -0300

    feat: add user registration endpoint and update UserDTO for JSON property access

--- src/main/java/com/ximple/library/controller/UserController.java
+++ src/main/java/com/ximple/library/controller/UserController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,12 @@ public class UserController {
     @Operation(summary = "Get all users", description = "Get all users in the library")
     public ResponseEntity<List<UserDTO>> findAll() {
         return ResponseEntity.ok(userService.findAll());
+    }
+
+    @PostMapping("")
+    @Operation(summary = "Register user", description = "Register a new user")
+    public ResponseEntity<Void> registerUser(UserDTO userDTO) {
+        userService.registerUser(userDTO);
+        return ResponseEntity.ok().build();
     }
 }
