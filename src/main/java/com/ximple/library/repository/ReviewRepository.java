72b5f92af50a30ffa36a0d9fab7a1952596cd0d3
vinicius <niviciusdev@gmail.com>
2025-03-08 04:17:29 -0300

    feat: add method to find reviews by book ID in ReviewRepository

--- src/main/java/com/ximple/library/repository/ReviewRepository.java
+++ src/main/java/com/ximple/library/repository/ReviewRepository.java
@@ -4,8 +4,12 @@ import com.ximple.library.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+
+    List<Review> findByBookId(UUID bookId);
+
 }
