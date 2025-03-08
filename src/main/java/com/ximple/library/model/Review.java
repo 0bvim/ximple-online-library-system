cce58eef173bb18344f9fb1620e28a0311220533
vinicius <niviciusdev@gmail.com>
2025-03-08 01:20:17 -0300

    feat: add OpenAPI definition to LibraryApplication for API documentation

--- src/main/java/com/ximple/library/model/Review.java
+++ src/main/java/com/ximple/library/model/Review.java
@@ -13,14 +13,12 @@ import java.util.UUID;
 
 public record Review(
         @Id
-        @GeneratedValue(strategy = GenerationType.UUID)
         UUID id,
         @NotEmpty(message = "Book ID is required")
         UUID bookId,
         @NotEmpty(message = "User ID is required")
         UUID userId,
-        @Min(1)
-        @Max(5)
+        @Min(1) @Max(5)
         int rating,
         @Size(max = 200, message = "Comment should be less than 200 characters")
         String comment,
