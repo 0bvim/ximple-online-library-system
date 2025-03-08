55278445d2cf283f4dac404c432f8f45cdba5f0c
vinicius <niviciusdev@gmail.com>
2025-03-08 16:34:17 -0300

    feat: add Javadoc comments to BookRepository methods for improved documentation

--- src/main/java/com/ximple/library/repository/BookRepository.java
+++ src/main/java/com/ximple/library/repository/BookRepository.java
@@ -1,3 +1,6 @@
+/**
+ * Repository interface for managing Book entities.
+ */
 package com.ximple.library.repository;
 
 import com.ximple.library.model.Book;
@@ -9,18 +12,56 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing Book entities.
+ */
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
+  /**
+   * Decrements the available copies of a book by 1 if there are available copies.
+   *
+   * @param id the UUID of the book
+   * @return the number of rows affected
+   */
   @Modifying
   @Query("UPDATE Book b SET b.amount = b.amount - 1 WHERE b.id = :id AND b.amount > 0")
   int decrementAvailableCopies(@Param("id") UUID id);
 
+  /**
+   * Increments the available copies of a book by 1.
+   *
+   * @param id the UUID of the book
+   */
   @Modifying
   @Query("UPDATE Book b SET b.amount = b.amount + 1 WHERE b.id = :id")
   void incrementAvailableCopies(@Param("id") UUID id);
 
+  /**
+   * Finds books by their title.
+   *
+   * @param title the title of the book
+   * @return a list of books with the given title
+   */
   List<Book> findByTitle(String title);
+  /**
+   * Finds books by their author.
+   *
+   * @param author the author of the book
+   * @return a list of books by the given author
+   */
   List<Book> findByAuthor(String author);
+  /**
+   * Finds books by their ISBN.
+   *
+   * @param isbn the ISBN of the book
+   * @return a list of books with the given ISBN
+   */
   List<Book> findByIsbn(String isbn);
+  /**
+   * Finds books by their genre.
+   *
+   * @param genre the genre of the book
+   * @return a list of books with the given genre
+   */
   List<Book> findByGenre(String genre);
 }
