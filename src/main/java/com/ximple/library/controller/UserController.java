c265fea379b56a62a1639d6d372c2f75d876a8e2
vinicius <niviciusdev@gmail.com>
2025-03-08 02:24:15 -0300

    feat: implement User model, controller, service, repository, and DTO for user management

---
+++ src/main/java/com/ximple/library/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.ximple.library.controller;
+
+import com.ximple.library.dto.UserDTO;
+import com.ximple.library.repository.UserRepository;
+import com.ximple.library.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Tag(name = "User", description = "Operations pertaining to users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    @Operation(summary = "Get all users", description = "Get all users in the library")
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+}
---
+++ src/main/java/com/ximple/library/service/UserService.java
@@ -0,0 +1,38 @@
+package com.ximple.library.service;
+
+import com.ximple.library.dto.UserDTO;
+import com.ximple.library.model.User;
+import com.ximple.library.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        log.info("UserService initialized");
+    }
+
+    public void registerUser(UserDTO userDTO) {
+        log.debug("Registering user: {}", userDTO);
+        User user = User.fromDTO(userDTO);
+        userRepository.save(user);
+        log.info("User registered: {}", user);
+    }
+
+    public List<UserDTO> findAll() {
+        log.debug("Fetching all users");
+        return userRepository.findAll()
+                .stream()
+                .map(User::toDTO)
+                .toList();
+    }
+}
