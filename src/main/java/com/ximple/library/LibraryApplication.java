007c3a4790a541a9a3468a96abc2fa549c691771
vinicius <niviciusdev@gmail.com>
2025-03-07 23:36:57 -0300

    feat: enable JPA repositories in LibraryApplication

--- src/main/java/com/ximple/library/LibraryApplication.java
+++ src/main/java/com/ximple/library/LibraryApplication.java
@@ -1,15 +1,11 @@
 package com.ximple.library;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class LibraryApplication {
 
 	public static void main(String[] args) {
