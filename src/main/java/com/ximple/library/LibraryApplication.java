1eb7d2d7ebfc9b02ada39f0857dab59cc49ac2f1
vinicius <niviciusdev@gmail.com>
2025-03-08 01:20:15 -0300

    feat: add OpenAPI definition to LibraryApplication for API documentation

--- src/main/java/com/ximple/library/LibraryApplication.java
+++ src/main/java/com/ximple/library/LibraryApplication.java
@@ -1,11 +1,17 @@
 package com.ximple.library;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@OpenAPIDefinition(
+		info = @Info(title = "Online Library API", version = "1.0", description = "Online library system with book search, reservations, and reviews")
+)
+
 public class LibraryApplication {
 
 	public static void main(String[] args) {
