d23af4686e62dbd9ccf19c41145c13eeb932fb10
vinicius <niviciusdev@gmail.com>
2025-03-08 09:23:23 -0300

    feat: implement ReservationController and ReservationService for managing book reservations

---
+++ src/main/java/com/ximple/library/repository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package com.ximple.library.repository;
+
+import com.ximple.library.dto.ReservationDTO;
+import com.ximple.library.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+
+}
