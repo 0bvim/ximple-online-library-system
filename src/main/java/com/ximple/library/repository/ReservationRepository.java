cf91f9a616423dac1e42687d2d236861c3bca254
vinicius <niviciusdev@gmail.com>
2025-03-08 13:26:13 -0300

    fix: remove unused import in ReservationRepository

--- src/main/java/com/ximple/library/repository/ReservationRepository.java
+++ src/main/java/com/ximple/library/repository/ReservationRepository.java
@@ -5,7 +5,6 @@ import com.ximple.library.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
